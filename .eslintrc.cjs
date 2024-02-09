0a8bf84ecf6cf549b4ae64db996c0e40c1914e6e
Hizkia Reppi <hizkiajefren0501@gmail.com>
2024-02-09 23:06:14 +0800

    chore: add rules on eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,25 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-unused-vars': 'warn',
+    'linebreak-style': 'off',
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
   }
 }
